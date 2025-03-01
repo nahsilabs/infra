cefe145204c19389aa660289ae2e4daec2775b82
Anatolios Laskaris <github_me@nahsi.dev>
2025-03-01 18:38:24 +0200

    More local services

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,9 +1,12 @@
 resource "adguard_rewrite" "haproxy" {
   for_each = toset([
     "proxmox",
-    "adguard",
-    "ceph-dashboard",
     "opnsense",
+    "adguard",
+    "unifi",
+    "power-panel",
+    "valetudo",
+    "ceph",
     "ipmi",
   ])
   domain = "${each.value}.nahsi.dev"
@@ -13,4 +16,18 @@ resource "adguard_rewrite" "haproxy" {
 resource "adguard_rewrite" "nfs" {
   domain = "nfs.local"
   answer = "10.2.13.40"
+}
+
+resource "cloudflare_record" "torrents" {
+  zone_id = data.cloudflare_zone.nahsi.zone_id
+  name    = "torrent.nahsi.dev"
+  content = "37.98.199.189"
+  type    = "A"
+}
+
+resource "cloudflare_record" "vpn" {
+  zone_id = data.cloudflare_zone.nahsi.zone_id
+  name    = "vpn.nahsi.dev"
+  content = "37.98.199.180"
+  type    = "A"
 }
