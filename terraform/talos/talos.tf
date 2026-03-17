5fa43f0f87bac6f15e95cbbe0d6f8e6ac67b46d8
Anatolios Laskaris <nahsi@nahsi.dev>
2026-03-17 19:53:41 +0200

    feat: Deploy envoy gateway (#230)

--- terraform/talos/talos.tf
+++ terraform/talos/talos.tf
@@ -64,7 +64,6 @@ resource "talos_cluster_kubeconfig" "this" {
   depends_on           = [talos_machine_bootstrap.this]
   client_configuration = talos_machine_secrets.this.client_configuration
   node                 = var.control_planes[0].server_ip
-  endpoint             = local.cluster_endpoint
   timeouts = {
     create = "3m"
   }
